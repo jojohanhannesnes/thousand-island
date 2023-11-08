c72d86c4b1339eac9267d350dfe792558df540b9
johannes sandjaja <johannes@johanness-MacBook-Pro.local>
2023-11-09 03:00:57 +0700

    euler 1

---
+++ src/euler/multiple_3_or_5.rs
@@ -0,0 +1,12 @@
+// https://projecteuler.net/problem=1
+
+fn sum_multiple_3_or_5(limit: i32) -> i32 {
+    (1..limit)
+        .filter(|x| (x % 3 == 0) || (x % 5 == 0))
+        .sum::<i32>()
+}
+
+#[test]
+fn test() {
+    assert_eq!(sum_multiple_3_or_5(1000), 233168)
+}
