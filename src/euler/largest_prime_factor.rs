317d2c93358c8bc78abfecd03ba66c85f643d5ed
johannes sandjaja <johannes@johanness-MacBook-Pro.local>
2023-11-10 01:40:01 +0700

    euler 3

---
+++ src/euler/largest_prime_factor.rs
@@ -0,0 +1,25 @@
+// https://projecteuler.net/problem=3
+
+fn largest_prime_factor(mut input: i64) -> i64 {
+    let mut start: i64 = 2; // first prime number
+    let mut res = Vec::new();
+    // sqrt(32) = 2 * 2 sqrt(8) -> 2,  2 * 2 sqrt(2) -> 2 * 2 , sqrt(2)
+    while start.pow(2) <= input {
+        if input % start == 0 {
+            res.push(start);
+            input /= start;
+        } else {
+            start += 1;
+        }
+    }
+    if input > 1 {
+        res.push(input); // last number inside the square root
+    }
+
+    res.last().unwrap().to_owned()
+}
+
+#[test]
+fn test() {
+    assert_eq!(largest_prime_factor(600851475143), 6857)
+}
--- src/euler/mod.rs
+++ src/euler/mod.rs
@@ -1,2 +1,3 @@
 pub mod even_fib_numbers;
+pub mod largest_prime_factor;
 pub mod multiple_3_or_5;
