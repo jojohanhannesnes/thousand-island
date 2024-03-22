d857c78182a7f68e46c7904c6fc49cce4791b855
jojohanhannesnes <johannessandjaja@proton.me>
2024-03-22 16:09:41 +0700

    roman to int update

--- leetcode/src/roman_to_int.rs
+++ leetcode/src/roman_to_int.rs
@@ -1,21 +1,52 @@
 fn roman_to_int(s: String) -> i32 {
-    let mut c = s.chars();
-    while let Some(s) = c.next() {
-        // if char == 'b' {
-        //     it.nth(1); // nth(1) skips/consumes exactly 2 items
-        //     continue;
-        // }
-        // print!("{}", char);
-        match s {
-            'I' => 1,
+    let mut total = 0;
+    let mut roman = s.chars().peekable();
+    while let Some(c) = roman.next() {
+        println!("{c}");
+        let value = match c {
+            'I' => {
+                let next = roman.peek();
+                if next == Some(&'V') {
+                    roman.next();
+                    4
+                } else if next == Some(&'X') {
+                    roman.next();
+                    9
+                } else {
+                    1
+                }
+            }
             'V' => 5,
-            'X' => 10,
+            'X' => {
+                let next = roman.peek();
+                if next == Some(&'L') {
+                    roman.next();
+                    40
+                } else if next == Some(&'C') {
+                    roman.next();
+                    90
+                } else {
+                    10
+                }
+            }
             'L' => 50,
-            'C' => 100,
+            'C' => {
+                let next = roman.peek();
+                if next == Some(&'D') {
+                    roman.next();
+                    400
+                } else if next == Some(&'M') {
+                    roman.next();
+                    900
+                } else {
+                    100
+                }
+            }
             'D' => 500,
             'M' => 1000,
             _ => continue,
         };
+        total += value;
     }
-    1
+    total
 }
